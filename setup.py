a06ed52cda411794dff89b4d34ce331b226a642b
Scott Kruger <scott@chojin.org>
2015-06-09 16:49:06 -0700

    Add install requirements

--- setup.py
+++ setup.py
@@ -4,5 +4,11 @@ if __name__ == '__main__':
     setup(
         name='pylastfm',
         version='0.1',
-        packages=find_packages(exclude=['tests'])
+        packages=find_packages(exclude=['tests']),
+        install_requires=[
+            'requests>=2.5.1',
+            'six>=1.9.0',
+            'python-dateutil>=2.4.0',
+            'figgis>=1.6.0',
+        ],
     )
