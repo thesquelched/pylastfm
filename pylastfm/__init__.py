b1cbe04b9c92a72ca2e29ef538a6719380568627
Scott Kruger <scott@chojin.org>
2015-06-11 13:23:42 -0700

    Add version info

--- pylastfm/__init__.py
+++ pylastfm/__init__.py
@@ -4,7 +4,9 @@ Python LastFM bindings
 
 from .client import LastFM
 from .error import LastfmError, AuthenticationError, APIError, FileError
+from . import _version
 
 
 __all__ = ['LastFM', 'LastfmError', 'AuthenticationError', 'APIError',
            'FileError']
+__version__ = _version.__version__
--- setup.py
+++ setup.py
@@ -1,9 +1,23 @@
 from setuptools import setup, find_packages
+import os.path
+
+
+def read_version():
+    """Read the library version"""
+    path = os.path.join(
+        os.path.abspath(os.path.dirname(__file__)),
+        'pylastfm',
+        '_version.py'
+    )
+    with open(path) as f:
+        exec(f.read())
+        return locals()['__version__']
+
 
 if __name__ == '__main__':
     setup(
         name='pylastfm',
-        version='0.1',
+        version=read_version(),
         packages=find_packages(exclude=['tests']),
         install_requires=[
             'requests>=2.5.1',
