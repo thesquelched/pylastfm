4e7ed4c724ff949d195a76f75d6566837cc426ff
Scott Kruger <scott@chojin.org>
2016-02-10 11:14:43 -0600

    Expand project metadata

--- setup.py
+++ setup.py
@@ -14,10 +14,51 @@ def read_version():
         return locals()['__version__']
 
 
+def download_url():
+    return 'https://github.com/thesquelched/pylastfm/tarball/{0}'.format(
+        read_version())
+
+
+CHANGELOG = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)),
+    'CHANGELOG.md')
+
+DESCRIPTION = """\
+pylastfm
+========
+
+Lasty Last.FM API bindings
+
+.. image:: https://travis-ci.org/thesquelched/pylastfm.svg?branch=master
+    :target: https://travis-ci.org/thesquelched/pylastfm
+
+`GitHub Project <https://github.com/thesquelched/pylastfm>`_"""
+
+
 if __name__ == '__main__':
+    try:
+        with open(CHANGELOG) as f:
+            changelog = f.read().strip()
+            suffix = """
+
+Changelog
+---------
+{0}""".format(changelog)
+    except IOError:
+        suffix = ''
+
     setup(
         name='pylastfm',
         version=read_version(),
+
+        description='Lazy Last.FM API bindings',
+        long_description=DESCRIPTION + suffix,
+
+        author='Scott Kruger',
+        author_email='scott@chojin.org',
+        url='https://github.com/thesquelched/pylastfm',
+        download_url=download_url(),
+
         packages=find_packages(exclude=['tests']),
         install_requires=[
             'requests>=2.5.1',
@@ -25,4 +66,15 @@ if __name__ == '__main__':
             'python-dateutil>=2.4.0',
             'figgis>=1.6.0',
         ],
+
+        classifiers=[
+            'Development Status :: 3 - Alpha',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: Apache Software License',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python :: 3.2',
+            'Programming Language :: Python :: 3.3',
+            'Programming Language :: Python :: 3.4',
+            'Topic :: Software Development :: Libraries',
+        ]
     )
